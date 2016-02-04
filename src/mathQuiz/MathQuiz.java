ffcda645da3d31645f9de4225eb6289cb03fb50d
webRookie <dumitriucristian@yahoo.com>
2016-02-04 10:58:21 +0200

    # WARNING: head commit changed in the meantime
    test

--- src/mathQuiz/MathQuiz.java
+++ src/mathQuiz/MathQuiz.java
@@ -6,9 +6,11 @@ public class MathQuiz {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*test */
 		Addition quiz = new Addition();
 		int result = quiz.validResult(15, 25);
-		System.out.println(result + "test 2 + 2 + 2 + 2+e");
+		System.out.println(result + "test 2 + 2 + 2 + 2+e"
+				+ "");
 		System.out.println(quiz.finalFailedMessage(result));
 		
 		
